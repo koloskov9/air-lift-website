48236c31e7ed75d451c9462bf744906bd00f8e3a
koloskov9 <koloskov9@gmail.com>
2026-06-15 19:18:42 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,3 +25,14 @@
     });
 })();
 */
+(function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
+})();
